cb81d88e9ddc81ddea5fe52376c37a14b0afe060
ItsgalinaL <S.galina.l@yandex.ru>
2023-07-22 13:48:41 +0700

    Выполнение задания

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,64 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = Faker.instance();
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto user = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            sendRequest(user);
+            return user;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
---
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -0,0 +1,91 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.data.DataGenerator.Registration.getUser;
+import static ru.netology.data.DataGenerator.getRandomLogin;
+import static ru.netology.data.DataGenerator.getRandomPassword;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        System.out.println(registeredUser.getLogin());
+        System.out.println(registeredUser.getPassword());
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        $(new By.ByTagName("h2")).shouldHave(Condition.partialText("Личный кабинет"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        SelenideElement notification = $("[data-test-id=error-notification]");
+        notification.$("[class=notification__content]").shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        SelenideElement notification = $("[data-test-id=error-notification]");
+        notification.$("[class=notification__content]").shouldHave(Condition.exactText("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(wrongLogin);
+        form.$("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        SelenideElement notification = $("[data-test-id=error-notification]");
+        notification.$("[class=notification__content]").shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(wrongPassword);
+        form.$("[data-test-id=action-login]").click();
+
+        SelenideElement notification = $("[data-test-id=error-notification]");
+        notification.$("[class=notification__content]").shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+}
